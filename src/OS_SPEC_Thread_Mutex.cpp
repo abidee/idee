78b94708dd95888dd69066ea7fe33b883c35c173
abidee xu <abidee.xu@gmail.com>
2013-01-31 18:21:36 +0800

    add file

---
+++ src/OS_SPEC_Thread_Mutex.cpp
@@ -0,0 +1,58 @@
+﻿#include "OS_SPEC_Thread_Mutex.h"
+
+//namespace ACE_OS_SPEC
+//{
+	
+
+int thread_mutex_lock(ACE_thread_mutex_t *m)
+{
+  ::EnterCriticalSection (m);
+  	return 0;
+}
+
+
+int thread_mutex_unlock (ACE_thread_mutex_t *m)
+{
+  ::LeaveCriticalSection (m);
+  return 0;
+}
+
+
+int thread_mutex_trylock (ACE_thread_mutex_t *m)
+{
+	BOOL result = ::TryEnterCriticalSection (m);
+  if (result == TRUE)
+    return 0;
+  else
+    {
+      //errno = EBUSY;
+      return -1;
+    }
+}
+
+
+int thread_mutex_init (ACE_thread_mutex_t *m, int lock_type, const char *name, void *arg)  //ACE_mutexattr_t
+{  
+    ::InitializeCriticalSection (m);   
+  	return 0;
+}
+
+
+int thread_mutex_destroy (ACE_thread_mutex_t *m)
+{
+	::DeleteCriticalSection (m);
+    return 0;
+}
+
+#if defined(ACE_WIN_PLATFORM)
+
+#elif defined(ACE_LINUX_PLATFORM) 
+
+ACE_INLINE int thread_mutex_lock(ACE_thread_mutex_t *m)
+{
+  pthread_mutex_lock(m);
+}
+
+#endif
+
+//}
---
+++ src/OS_SPEC_Thread_Mutex.h
@@ -0,0 +1,37 @@
+﻿/**
+*	@file 
+* @brief 对不同操作系统的底层函数的封装
+* @author xq
+*/
+
+#ifndef OS_SPEC_THREAD_IMPL_H
+#define OS_SPEC_THREAD_IMPL_H
+
+#include "stdafx.h"
+
+//namespace ACE_OS_SPEC
+//{
+
+
+
+#if defined(ACE_WIN_PLATFORM)
+
+typedef CRITICAL_SECTION ACE_thread_mutex_t;
+
+#elif defined(ACE_LINUX_PLATFORM) 
+
+typedef pthread_mutex_t ACE_thread_mutex_t;
+
+#endif
+
+
+int thread_mutex_lock(ACE_thread_mutex_t *m);
+int thread_mutex_unlock (ACE_thread_mutex_t *m);
+int thread_mutex_trylock (ACE_thread_mutex_t *m);
+int thread_mutex_destroy (ACE_thread_mutex_t *m);
+int thread_mutex_init (ACE_thread_mutex_t *m, int lock_type, const char *name, void *arg);
+
+
+//}
+
+#endif
